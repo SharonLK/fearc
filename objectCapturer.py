245bbeff49f6c2a1bc3254c878f47414d264f0b0
chen <chenshooker@gmail.com>
2018-04-21 19:04:13 +0300

    open video from code

--- objectCapturer.py
+++ objectCapturer.py
@@ -1,18 +1,18 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(r'C:\Users\Chen\PycharmProjects\fearc\video\vid1.mp4')
+ret, frame = cap.read()
 
-while(True):
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-
+while frame is not None:
 
     # Display the resulting frame
     cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
+    # Capture frame-by-frame
+    ret, frame = cap.read()
 # When everything done, release the capture
 cap.release()
 cv2.destroyAllWindows()
