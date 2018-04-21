0199ab39c986ff9588f735e968b458ebbba87d58
Sharon <sharonkl@campus.technion.ac.il>
2018-04-21 18:51:45 +0300

    Remove, organize and rename according to python guidelines

--- object_capturer.py
+++ object_capturer.py
@@ -1,15 +1,13 @@
-import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
 
-while(True):
+while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-
     # Display the resulting frame
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
