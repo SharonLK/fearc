52650631e8a09904b479a32003911b25f4877b18
Sharon <sharonlevenzonkuninin@gmail.com>
2018-05-08 11:21:36 +0300

    Check for intersection with bounds and draw score

--- pong/game_better.py
+++ pong/game_better.py
@@ -15,12 +15,16 @@ BLACK = (0, 0, 0)
 class Pong:
     def __init__(self):
         self.ball_pos = (int(WIDTH / 2), int(HEIGHT / 2))
-        self.direction = (3, 3)
+        self.direction = (4, 4)
         self.pad1_pos = pygame.Rect(10, int(HEIGHT / 2), 10, 80)
         self.pad2_pos = pygame.Rect(WIDTH - 20, int(HEIGHT / 2), 10, 80)
 
         self.mousex = 0
         self.mousey = 0
+
+        self.score = (0, 0)
+
+        self.font = pygame.font.Font('freesansbold.ttf', 44)
 
     def update(self):
         # Move the ball in the specified direction
@@ -40,26 +44,45 @@ class Pong:
 
         # Check if the ball intersects with the left paddle
         if self.ball_pos[1] - RADIUS < self.pad1_pos.bottom and \
-                                self.ball_pos[1] + RADIUS > self.pad1_pos.top and \
-                                self.ball_pos[0] - RADIUS < self.pad1_pos.right and \
-                                self.ball_pos[0] + RADIUS > self.pad1_pos.left:
+                self.ball_pos[1] + RADIUS > self.pad1_pos.top and \
+                self.ball_pos[0] - RADIUS < self.pad1_pos.right and \
+                self.ball_pos[0] + RADIUS > self.pad1_pos.left:
             self.direction = (abs(self.direction[0]) * ACCELERATION, self.direction[1] * ACCELERATION)
 
-        # check if the ball intersects with the right paddle
+        # Check if the ball intersects with the right paddle
         if self.ball_pos[1] - RADIUS < self.pad2_pos.bottom and \
-                                self.ball_pos[1] + RADIUS > self.pad2_pos.top and \
-                                self.ball_pos[0] - RADIUS < self.pad2_pos.right and \
-                                self.ball_pos[0] + RADIUS > self.pad2_pos.left:
+                self.ball_pos[1] + RADIUS > self.pad2_pos.top and \
+                self.ball_pos[0] - RADIUS < self.pad2_pos.right and \
+                self.ball_pos[0] + RADIUS > self.pad2_pos.left:
             self.direction = (-abs(self.direction[0]) * ACCELERATION, self.direction[1] * ACCELERATION)
+
+        # Check if ball intersects with the right side of the board
+        if self.ball_pos[0] + 10 >= WIDTH:
+            self.score = (self.score[0] + 1, self.score[1])
+            self.direction = (4, 4)
+            self.ball_pos = (int(WIDTH / 2), int(HEIGHT / 2))
+
+        # Check if ball intersects with the left side of the board
+        if self.ball_pos[0] - 10 <= 0:
+            self.score = (self.score[0], self.score[1] + 1)
+            self.direction = (4, 4)
+            self.ball_pos = (int(WIDTH / 2), int(HEIGHT / 2))
 
     def draw(self):
         pygame.draw.circle(DISPLAY, WHITE, (int(self.ball_pos[0]), int(self.ball_pos[1])), RADIUS)
         pygame.draw.rect(DISPLAY, WHITE, self.pad1_pos)
         pygame.draw.rect(DISPLAY, WHITE, self.pad2_pos)
 
+        surf = self.font.render("{}".format(self.score[0]), True, (124, 174, 205))
+        DISPLAY.blit(surf, (100, 50))
+        surf = self.font.render("{}".format(self.score[1]), True, (124, 174, 205))
+        DISPLAY.blit(surf, (WIDTH - 100, 50))
+
 
 if __name__ == "__main__":
     global DISPLAY
+
+    pygame.init()
 
     FPS_CLOCK = pygame.time.Clock()
     DISPLAY = pygame.display.set_mode((WIDTH, HEIGHT))
