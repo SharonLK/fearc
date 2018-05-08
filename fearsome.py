d9b94bb53a2c26db3d2caa976d57c0fd8adc7f29
Sharon <sharonlevenzonkuninin@gmail.com>
2018-05-08 23:41:47 +0300

    Stop the program when Q or ESC are hit

--- fearsome.py
+++ fearsome.py
@@ -6,15 +6,18 @@ import pygame
 from object_capturer import objectCapturer
 from pong.game_better import Pong
 
+running = True
+
+
 def capturer_loop(capturer):
-    while not objectCapturer.is_video_done:
-        pointL, pointR = objectCapturer.get_locations()
+    while not objectCapturer.is_video_done and running:
+        pointL, pointR = capturer.get_locations()
 
         if pointL is not None:
-            transformedL = (pointL[1] / objectCapturer.calibrated_height) * 1080
+            transformedL = (pointL[1] / capturer.calibrated_height) * 1080
             pong.pad1_pos.center = (pong.pad1_pos.center[0], transformedL)
         if pointR is not None:
-            transformedR = (pointR[1] / objectCapturer.calibrated_height) * 1080
+            transformedR = (pointR[1] / capturer.calibrated_height) * 1080
             pong.pad2_pos.center = (pong.pad2_pos.center[0], transformedR)
 
 
@@ -28,7 +31,8 @@ if __name__ == "__main__":
     while True:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_q:
+                if event.key == pygame.K_q or event.key == pygame.K_ESCAPE:
+                    running = False
                     pygame.quit()
                     sys.exit()
 
